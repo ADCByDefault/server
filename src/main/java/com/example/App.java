66b3ae58f8439098d9efe23e2677b6620cc6019f
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 10:13:38 +0200

    implemented server

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import java.io.*;
+import java.net.*;
 /**
  * Hello world!
  *
@@ -8,6 +10,25 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        try {
+            // accendo il server
+            ServerSocket serverSocket = new ServerSocket(3000);
+
+            // metto in ascolto
+            Socket client = serverSocket.accept();
+            BufferedReader inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String stringRicevuta = inDalClient.readLine();
+
+            // invio al client
+            String daRitornare = stringRicevuta.toUpperCase();
+            DataOutputStream outVersoClient = new DataOutputStream(client.getOutputStream());
+            outVersoClient.writeBytes(daRitornare+"\n");
+            
+            System.out.println("ritornato : " + daRitornare);
+            serverSocket.close();
+            client.close();
+        } catch (Exception e) {
+            System.out.println("Errore");
+        }
     }
 }
