6425b56ad209924e2c7f0d317209ee0c4a68017e
lai2-01 <lai2-01@itismeucci.com>
2023-10-09 10:44:23 +0200

    fixed server close

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,22 +11,39 @@ public class App
     public static void main( String[] args )
     {
         try {
-            // accendo il server
-            ServerSocket serverSocket = new ServerSocket(3000);
+            ServerSocket serverSocket = null;
+            Socket client = null;
+            while(true){
 
-            // metto in ascolto
-            Socket client = serverSocket.accept();
-            BufferedReader inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            String stringRicevuta = inDalClient.readLine();
+                // accendo il server
+                serverSocket = new ServerSocket(3000);
 
-            // invio al client
-            String daRitornare = stringRicevuta.toUpperCase();
-            DataOutputStream outVersoClient = new DataOutputStream(client.getOutputStream());
-            outVersoClient.writeBytes(daRitornare+"\n");
+                // metto in ascolto
+                System.out.println("sono in ascolto");
+                client = serverSocket.accept();
+                BufferedReader inDalClient = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                String stringRicevuta = inDalClient.readLine();
+
+                // invio al client
+                String daRitornare = stringRicevuta.toUpperCase();
+                DataOutputStream outVersoClient = new DataOutputStream(client.getOutputStream());
+                stringRicevuta =  stringRicevuta.trim();
+                if(stringRicevuta.equals("q")){
+                    System.out.println(stringRicevuta);
+                    outVersoClient.writeBytes("chiudo il server");
+                    serverSocket.close();
+                    client.close();
+                    break;
+                }
+
+                outVersoClient.writeBytes(daRitornare+"\n");
+                
+
+                System.out.println("ritornato : " + daRitornare);
+                serverSocket.close();
+                client.close();
+            }
             
-            System.out.println("ritornato : " + daRitornare);
-            serverSocket.close();
-            client.close();
         } catch (Exception e) {
             System.out.println("Errore");
         }
